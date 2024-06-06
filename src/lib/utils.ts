e51c80933a689d8c68900a3db678a20404b69d74
Raphael Schütz <raphael.schuetz0311@gmail.com>
2024-06-06 13:04:01 +0200

    Refactor utils.ts: Remove unused filterSortMap function

--- src/lib/utils.ts
+++
@@ -1,9 +0,0 @@
-function filterSortMap(array: any[], filter: any, sort: any, map: any) {
-    const out = [];
-    for (const item of array) {
-        if (filter(item)) {
-            out.push(map(item));
-        }
-    }
-    return out.sort(sort);
-}
