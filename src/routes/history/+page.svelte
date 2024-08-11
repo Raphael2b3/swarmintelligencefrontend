dca46ecc2eca8dfa608dababf78951b0ee9d4e20
Raphael2b3 <raphael.schuetz0311@gmail.com>
2024-08-11 11:23:52 +0200

    chore: Remove unused CSS styles, config files, and components

--- src/routes/history/+page.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-	import DiscoverView from '$lib/components/views/DiscoverView.svelte';
-	import { historyManager } from '$lib/state/history.svelte';
-</script>
-
-<DiscoverView results={historyManager.current}></DiscoverView>
