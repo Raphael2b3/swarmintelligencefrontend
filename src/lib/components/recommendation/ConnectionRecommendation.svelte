7aaecf51dcc9033555f6b2649c54f494206a78e9
Raphael2b3 <raphael.schuetz0311@gmail.com>
2024-08-20 21:44:59 +0200

    Refactor recommendation components: Improve code structure and update element styling

--- src/lib/components/recommendation/ConnectionRecommendation.svelte
+++ src/lib/components/recommendation/ConnectionRecommendation.svelte
@@ -8,7 +8,7 @@
 
 	let { connection }: { connection: IConnection } = $props();
 	let value = $state(0.5);
-	let value_indicator = $derived('' + value * 100 + '%');
+	let value_indicator = $derived((value * 100).toFixed(2) + '%');
 	const arg_type_str = connection.isProArgument ? 'Pro' : 'Contra';
 </script>
 
@@ -22,11 +22,15 @@
 </ActionButtonList>
 This is Actually a good {arg_type_str} Argument
 
-<div style="width:100%;  display: flex; align-items: center;">
-	bad
-	<div style="flex:1 ; display:flex; flex-direction: column; align-items: center;">
-		{value_indicator}
+<div
+	style="flex:1 ; display:flex; flex-direction: column; align-items: center; justify-content: space-around;"
+>
+	{value_indicator}
+
+	<div style="width:100%;  display: flex; align-items: center;">
+		bad
+
 		<Slider bind:value></Slider>
+		good
 	</div>
-	good
 </div>
