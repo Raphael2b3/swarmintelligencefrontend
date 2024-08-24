ae3bf884c9ed1bf60f186ea70fab82274dcc37df
Raphael2b3 <raphael.schuetz0311@gmail.com>
2024-08-24 16:09:26 +0200

    Co-authored-by: HlDE1 <HlDE1@users.noreply.github.com>

--- src/lib/components/ActionButtonList.svelte
+++ src/lib/components/ActionButtonList.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
 	import ExpandButton from './buttons/iconbuttons/ExpandButton.svelte';
 	import { scale } from 'svelte/transition';
 	import { quintOut } from 'svelte/easing';
@@ -6,13 +6,47 @@
 	$effect(() => {
 		console.log(expanded);
 	});
+
+	function accordion(node: any, isOpen: boolean) {
+		let initialHeight = node.offsetHeight;
+		node.style.height = isOpen ? 'auto' : 0;
+		node.style.overflow = 'hidden';
+		return {
+			update(isOpen) {
+				let animation = node.animate(
+					[
+						{
+							height: initialHeight + 'px',
+							overflow: 'hidden'
+						},
+						{
+							height: 0,
+							overflow: 'hidden'
+						}
+					],
+					{ duration: 200, fill: 'both' }
+				);
+				animation.pause();
+				if (!isOpen) {
+					animation.play();
+				} else {
+					animation.reverse();
+				}
+			}
+		};
+	}
 </script>
 
 <div class="container">
-	<ExpandButton bind:expanded></ExpandButton>
-	{#if expanded}
+	<div
+		use:accordion={expanded}
+		style="display: flex; 
+		gap: 10px; flex-direction: column;"
+	>
 		<slot></slot>
-	{/if}
+	</div>
+	<ExpandButton bind:expanded></ExpandButton>
+
 	<!-- Slot für dynamische Inhalte -->
 
 	<!-- Slot für dynamische Inhalte -->
@@ -21,15 +55,17 @@
 <style>
 	.container {
 		position: absolute;
-		background-color: rgba(148, 148, 148, 0.269);
+		/* background-color: rgba(148, 148, 148, 0.269); */
+		background-color: rgba(180, 180, 180, 0.304);
 		border-radius: 10000px;
+		height: calc-size(auto);
 		bottom: 20%;
 		right: 4%;
 		display: flex;
 		flex-direction: column;
-		align-items: flex-end;
-		gap: 10px; /* Abstand zwischen den Items */
+		align-items: flex-end; /* Abstand zwischen den Items */
 		z-index: 10; /* Stellt sicher, dass die Liste über dem Inhalt liegt */
-		transition: 200ms;
+		transition: height 200ms;
+		overflow: hidden;
 	}
 </style>
